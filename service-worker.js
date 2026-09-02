5f80c31b0d4a5d16456c493fb24f3ed1ede1d555
koh1koh2koh-cmyk <koh1koh2koh@gmail.com>
2026-09-03 00:09:23 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'schedule-app-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// تثبيت ملف الـ Service Worker وتخزين الملفات
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// تشغيل الملف وجلب الموارد من الكاش عند عدم وجود إنترنت
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
